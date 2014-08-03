4a25ab7103a7e78d667423979610e18ca17d80f6
jun85664396 <jun85664396@gmail.com>
2014-08-03 17:58:12 +0900

    keyup event add

--- src/main/webapp/resources/js/index/index.js
+++ src/main/webapp/resources/js/index/index.js
@@ -1,18 +1,22 @@
 require([ 'jquery', 'bootstrap', 'bootstrap-carousel' ], function($) {
-	$(function() {
-		console.log("test");
-		$("#submit").click(function() {
-			var data = {
-				"url" : $("#url").val()
-			};
-			$.ajax({
-				"url" : "/shortenURL",
-				"type" : "POST",
-				"data" : data,
-				"success" : function(res) {
-					$("#result").html("http://suw.kr/" + res);
-				}
-			});
-		});
-	});
+  $(function() {
+    $("#url").keyup(function(key){
+      if(key.keyCode == 13){
+        $("#submit").click();
+      }           
+    }); 
+    $("#submit").click(function() {
+      var data = { 
+        "url" : $("#url").val()
+      };  
+      $.ajax({
+        "url" : "/shortenURL",
+        "type" : "POST",
+        "data" : data,
+        "success" : function(res) {
+          $("#result").html("suw.kr/" + res);
+        }   
+      }); 
+    }); 
+  }); 
 });
